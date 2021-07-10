9303458fa8f4426ad469266637554a56c514c02d
Serhii2021 <86842862+Serhii2021@users.noreply.github.com>
2021-07-10 17:48:11 +0300

    new site

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// $(document).ready(function(){
+//    $('.slider').slick({
+//      setting-name: setting-value
+//    });
+//  });
+
+$(function () {
+
+   $('.slider').slick({
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      fade: true,
+      infinite: true,
+      autoplaySpeed:5000,
+      speed: 2000,
+      cssEase: 'linear',
+     
+      
+      
+
+   });
+});
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// var slides = document.querySelectorAll('slider .slider__item');
+// var currentSlide = 0;
+// var slideInterval = setInterval(nextSlide,2000);
+
+// function nextSlide(){
+//   slides[currentSlide].className = 'slider__item';
+//   currentSlide = (currentSlide+1)%slides.length;
+//   slides[currentSlide].className = 'slider__item showing';
+// }
